97efad52bdb63c9dfb2c2be11fd7eefc22458979
slypelican <git@slypelican.com>
2025-03-08 14:04:47 -0800

    Move index.js to ./src/

--- dist/index.js
+++ dist/index.js
@@ -3,11 +3,5 @@ import ReactDOM, { createRoot } from 'react-dom/client';
 // import './styles.css';
 
 import App from './mockup/App';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        <App>
-        </App>
-    </React.StrictMode>
-);
+root.render(/*#__PURE__*/React.createElement(React.StrictMode, null, /*#__PURE__*/React.createElement(App, null)));
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, { StrictMode } from 'react';
+import ReactDOM, { createRoot } from 'react-dom/client';
+// import './styles.css';
+
+import App from '../mockup/App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App>
+        </App>
+    </React.StrictMode>
+);
