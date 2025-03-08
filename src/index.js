72493dc77105adbc409465c3fb0c5e67b5122135
slypelican <git@slypelican.com>
2025-03-08 14:11:09 -0800

    Minor bug fixes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { StrictMode } from 'react';
 import ReactDOM, { createRoot } from 'react-dom/client';
 // import './styles.css';
 
-import App from '../mockup/App';
+import App from '../mockup/App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
