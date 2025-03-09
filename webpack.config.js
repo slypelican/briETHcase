3219aa8cf2344274d6b4f28cd58eb7ec442d96e7
slypelican <git@slypelican.com>
2025-03-08 18:45:00 -0800

    Fixed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,50 @@
 const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 module.exports = {
+    mode: "development",
+    entry: path.resolve(__dirname, 'src/index.js'),
     output: {
-      path: path.resolve(__dirname, 'build'),
-      filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
     },
     resolve: {
-      modules: [path.join(__dirname, 'src'), 'node_modules'],
-      alias: {
-        react: path.join(__dirname, 'node_modules', 'react'),
-      },
+        modules: [path.join(__dirname, 'src'), 'node_modules'],
+        alias: {
+            react: path.join(__dirname, 'node_modules', 'react'),
+        },
     },
     module: {
       rules: [
         {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-          },
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+            },
         },
         {
-          test: /\.css$/,
-          use: [
+            test: /\.css$/,
+            use: [
             {
-              loader: 'style-loader',
+                loader: 'style-loader',
             },
             {
-              loader: 'css-loader',
+                loader: 'css-loader',
             },
           ],
         },
       ],
     },
-  };
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: 'src/index.html',
+        }),
+      ],
+    devServer: {
+        devMiddleware: {
+            index: true,
+            serverSideRender: true,
+            writeToDisk: true,
+        },
+    }
+};
