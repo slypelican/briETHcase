f89cf72e0f35ea081da45414bc27adb2e56a6efa
slypelican <200390504+slypelican@users.noreply.github.com>
2025-03-08 21:52:37 -0800

    Switch App.js data source to test_data.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,5 @@
-const GROUP_DATA = [
-    {
-        "name": "Binance",
-        "cards": [
-            {
-                "address": "0x1234567890abcdef1234567890abcdef12345678",
-                "new_in": false,
-                "new_out": true,
-            },
-            {
-                "address": "0x0987654321fedcba0987654321fedcba09876543",
-                "new_in": true,
-                "new_out": false,
-            },
-        ]
-    },
-    {
-        "name": "Coinbase",
-        "cards": [
-            {
-                "address": "0xabcdef1234567890abcdef1234567890abcdef12",
-                "new_in": true,
-                "new_out": false,
-            },
-        ]
-    }
-]
+// Data set FOR TESTING ONLY
+import {GROUP_DATA} from './mockup/test.js'
 
 export default function App() {
     return(<WatchBoard groups={GROUP_DATA} />)
---
+++ src/test_data.js
@@ -0,0 +1,27 @@
+export const GROUP_DATA = [
+    {
+        "name": "Binance",
+        "cards": [
+            {
+                "address": "0x1234567890abcdef1234567890abcdef12345678",
+                "new_in": false,
+                "new_out": true,
+            },
+            {
+                "address": "0x0987654321fedcba0987654321fedcba09876543",
+                "new_in": true,
+                "new_out": false,
+            },
+        ]
+    },
+    {
+        "name": "Coinbase",
+        "cards": [
+            {
+                "address": "0xabcdef1234567890abcdef1234567890abcdef12",
+                "new_in": true,
+                "new_out": false,
+            },
+        ]
+    }
+]
