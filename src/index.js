3fad25e6d3702e594eeefcfe08d64cb07cd9d095
slypelican <200390504+slypelican@users.noreply.github.com>
2025-03-09 15:29:12 -0700

    Set React strict mode per official docs recommendation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import ReactDOM, { createRoot } from 'react-dom/client';
 import './styles.css';
 
 import App from './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
