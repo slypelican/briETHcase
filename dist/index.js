826c78c9aa15f9c93e77f17536224494b6845f1e
slypelican <git@slypelican.com>
2025-03-08 18:16:28 -0800

    Mistaken commit

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-import React, { StrictMode } from 'react';
-import ReactDOM, { createRoot } from 'react-dom/client';
-// import './styles.css';
-
-import App from './mockup/App';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(/*#__PURE__*/React.createElement(React.StrictMode, null, /*#__PURE__*/React.createElement(App, null)));
