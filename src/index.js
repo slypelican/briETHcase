2780a700816a209a8b0893669706ff5e9d9d78e7
slypelican <git@slypelican.com>
2025-03-08 19:48:41 -0800

    Removed React StrictMode

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-import React, { StrictMode } from 'react';
+import React from 'react';
 import ReactDOM, { createRoot } from 'react-dom/client';
 // import './styles.css';
 
 import App from '../mockup/App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        <App>
-        </App>
-    </React.StrictMode>
+root.render(<App />
 );
